b845aaeaed727916d28db7f4f1f01534f9b31fa1
zyaakov6 <130306204+zyaakov6@users.noreply.github.com>
2024-08-07 14:26:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector("#harav").addEventListener("mouseover", function(){
+    this.style.color = "blue";
+})
+document.querySelector("#harav").addEventListener("mouseout", function(){
+    this.style.color = "black";
+})
