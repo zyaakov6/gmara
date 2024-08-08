44b68738b2083b905ac2817796d663ae2f5b5ae1
zyaakov6 <130306204+zyaakov6@users.noreply.github.com>
2024-08-08 04:45:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,33 @@ document.querySelector("#harav").addEventListener("mouseover", function(){
 document.querySelector("#harav").addEventListener("mouseout", function(){
     this.style.color = "black";
 })
+document.addEventListener('DOMContentLoaded', function() {
+    const audioLinks = document.querySelectorAll('.audio-link');
+    const audioPlayer = document.getElementById('audio');
+    
+    audioLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const audioSrc = this.getAttribute('href');
+            audioPlayer.setAttribute('src', audioSrc);
+            audioPlayer.play();
+        });
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+        var dropdown = document.querySelector('.nav-link.dropdown-toggle');
+        var menu = document.querySelector('.dropdown-menu');
+
+        dropdown.addEventListener('click', function (event) {
+            event.preventDefault();
+            menu.classList.toggle('show');
+        });
+
+        document.addEventListener('click', function (event) {
+            if (!dropdown.contains(event.target) && !menu.contains(event.target)) {
+                menu.classList.remove('show');
+            }
+        });
+    });
+
+
+});
